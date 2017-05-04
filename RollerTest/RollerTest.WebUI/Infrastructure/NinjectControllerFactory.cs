f1f751b40a7b94ee72748b54a18ae22cad208c18
WangHuan <wanghuan_kf@smec-cn.com>
2017-05-04 10:19:30 +0800

    20170504：创建项目主框架

---
+++ RollerTest/RollerTest.WebUI/Infrastructure/NinjectControllerFactory.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Ninject;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace RollerTest.WebUI.Infrastructure
+{
+    public class NinjectControllerFactory:DefaultControllerFactory
+    {
+        private IKernel ninjectKernel;
+        public NinjectControllerFactory()
+        {
+            ninjectKernel = new StandardKernel();
+            AddBinding();
+        }
+        protected override IController GetControllerInstance(RequestContext requestContext,Type controllerType)
+        {
+            return controllerType == null ? null : (IController)ninjectKernel.Get(controllerType);
+        }
+
+        private void AddBinding()
+        {
+            //添加绑定
+        }
+    }
+}
---
+++ RollerTest/RollerTest.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RollerTest.WebUI.Startup))]
+namespace RollerTest.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

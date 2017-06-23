54ac663a7428952a84761e7c3acd6420c7622cd1
yezhiqiu11 <176017295@qq.com>
2017-06-23 14:52:35 +0800

    Merge remote-tracking branch 'refs/remotes/beingWH/master' into dev
    # Conflicts:
    #	RollerTest/RollerTest.Domain/RollerTest.Domain.csproj
    #	RollerTest/RollerTest.WebUI/RollerTest.WebUI.csproj
    #	RollerTest/RollerTest.WebUI/Startup.cs
    #	RollerTest/RollerTest.WebUI/Web.config

---
+++ RollerTest/RollerTest.WebUI/Controllers/SampleDecideController.cs
@@ -0,0 +1,54 @@
+﻿using RollerTest.Domain.Abstract;
+using RollerTest.Domain.Entities;
+using RollerTest.WebUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RollerTest.WebUI.Controllers
+{
+    public class SampleDecideController : Controller
+    {
+        private ITestreportinfoRepository testreportinfoRepo;
+        public SampleDecideController(ITestreportinfoRepository repo)
+        {
+            testreportinfoRepo = repo;
+        }
+
+        //public ActionResult Index()
+        //{
+
+        //    return View();
+        //}
+
+        //public ActionResult SampleDecide(int RollerSampleInfoId)
+        //{
+        //    SampleDecideViewModel sampledecideviewModel = new SampleDecideViewModel()
+        //    {
+        //        rollertestreportinfos = testreportinfoRepo.RollerTestreportInfos.Where(x => x.RollerSampleInfoID == RollerSampleInfoId)
+        //    };
+        //    return View(sampledecideviewModel);
+        //}
+        [HttpPost]
+        public ActionResult SampleDecide(RollerTestreportInfo rollertestreportinfo)
+        {
+            testreportinfoRepo.SaveRollerTestreportInfo(rollertestreportinfo);
+            //return RedirectToAction("SampleDecide", new { RollerSampleInfoID = rollertestreportinfo.RollerSampleInfoID });
+            //return RedirectToAction("../TestBlock/Index", new { RollerSampleInfoID = rollertestreportinfo.RollerSampleInfoID });
+            return RedirectToAction("../TestBlock/Index");
+        }
+        [HttpGet]
+        public ViewResult SampleDecide(int RollerSampleInfoID)
+        {
+            //SampleDecideViewModel sampledecideviewModel = new SampleDecideViewModel()
+            //{
+            //    rollertestreportinfos = testreportinfoRepo.RollerTestreportInfos.Where(x => x.RollerSampleInfoID == RollerSampleInfoID)
+            //};
+            RollerTestreportInfo rollertestreportinfo = testreportinfoRepo.RollerTestreportInfos.FirstOrDefault(x => x.RollerSampleInfoID == RollerSampleInfoID);
+            return View(rollertestreportinfo);
+        }
+       
+    }
+}
---
+++ RollerTest/RollerTest.WebUI/Models/SampleDecideViewModel.cs
@@ -0,0 +1,19 @@
+﻿using RollerTest.Domain.Abstract;
+using RollerTest.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RollerTest.WebUI.Models
+{
+    public class SampleDecideViewModel
+    {
+        private ITestreportinfoRepository testreportinfoRepo;
+
+        //public IEnumerable<RollerSampleInfo> rollersampleinfos { get; set; }
+        public IEnumerable<RollerTestreportInfo> rollertestreportinfos { get; set; }
+
+    }
+}
